8f028d01f5fbc95ce180ffd458d8aec90c6b19bb
sof202 <s.o.fletcher@exeter.ac.uk>
2026-08-20 15:07:53 +0100

    feat: add checksum check

--- lua/scrawl.lua
+++ lua/scrawl.lua
@@ -1,5 +1,6 @@
 local plugin_name = "scrawl.nvim"
 local scrawl_repo = "sof202/scrawl"
+local scrawl_tar = "scrawl-linux-glibc-x86_64.tar.gz"
 local plugin_binary_directory = vim.fn.stdpath("data") .. "/" .. plugin_name .. "/bin/"
 local M = {}
 
@@ -25,31 +26,76 @@ local function binary_location()
     return nil
 end
 
+-- Obtains checksum for scrawl tar from GitHub's REST API
+--- @param tag string
+--- @return string algorithm, string checksum
+local function get_checksum(tag)
+    local scrawl_api_url = "https://api.github.com/repos/" .. scrawl_repo
+    local release_url = scrawl_api_url .. "/releases/tags/" .. tag
+
+    -- 1. Hit GitHub REST API
+    if vim.fn.executable("curl") == 0 then
+        error("curl is not on PATH. Couldn't download scrawl.")
+    end
+    local curl_cmd = string.format("curl -sL '%s'", release_url)
+    local handle = io.popen(curl_cmd)
+    if handle == nil then
+        error("Failed to obtain checksum")
+    end
+    local response = handle:read("*a")
+    handle:close()
+
+    -- 2. Extract digest from JSON response
+    local algorithm, checksum = response:match('"digest"%s*:%s*"(%w+):(%x+)"')
+
+    if not checksum then
+        error("Failed to obtain checksum")
+    end
+
+    return algorithm, checksum
+end
+
 -- Obtains statically linked binary from GitHub releases and places binary
 -- into scrawl.nvim's managed binary path
 --- @param tag string
 local function download_binary_from_github(tag)
-    print(plugin_binary_directory)
     vim.fn.mkdir(plugin_binary_directory, "p")
 
     -- Note that only the glibc version is built currently
     local scrawl_repo_url = "https://github.com/" .. scrawl_repo
-    local tar_name = "scrawl-linux-glibc-x86_64.tar.gz"
-    local download_url = scrawl_repo_url .. "/releases/download/" .. tag .. "/" .. tar_name
+    local download_url = scrawl_repo_url .. "/releases/download/" .. tag .. "/" .. scrawl_tar
 
-    -- 1. Download
+    -- 1. Checksum valiation
     if vim.fn.executable("curl") == 0 then
         error("curl is not on PATH. Couldn't download scrawl.")
     end
+    local checksum_curl_cmd = string.format("curl -sL '%s' | sha256sum", download_url)
+    local handle = io.popen(checksum_curl_cmd)
+    if handle == nil then
+        error("curl: Failed to obtain checksum")
+    end
+    local output = handle:read("*a")
+    handle:close()
+    local actual_checksum = output:match("^([%x]+)")
 
-    download_location = plugin_binary_directory .. tar_name
-    vim.notify("Downloading:" .. tar_name, vim.log.levels.INFO)
-    local curl_cmd = string.format(
+    local _, expected_checksum = get_checksum(tag)
+    if actual_checksum ~= expected_checksum then
+        error(string.format(
+            "Checksums do not match (expected %s, actual %s)",
+            expected_checksum,
+            actual_checksum
+        ))
+    end
+
+    -- 1. Download
+    local download_location = plugin_binary_directory .. scrawl_tar
+    vim.notify("Downloading:" .. scrawl_tar, vim.log.levels.INFO)
+    local download_curl_cmd = string.format(
         "curl -sL -o '%s' '%s'",
         download_location,
         download_url
     )
-    local success, exit_code, _ = os.execute(curl_cmd)
+    local success, _, _ = os.execute(download_curl_cmd)
     if not success then
         error("curl failed to download tar from " .. download_url)
     end
